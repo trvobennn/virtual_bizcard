c01ca9ee167f7585cb7dee30aad0c21e957f5309
NB67TRV <trevor.andrew.bennett@gmail.com>
2022-12-01 14:02:33 -0500

    Add icons and fixed styling
    Added github and linkedin icons to footer
    Fixed minor style issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
+import giticon from "./githublight.png";
+import linkedicon from "./linkedinwhite.png";
 
 function Headshot() {
   return (
@@ -25,15 +27,25 @@ function Mainbox(){
 }
 
 function Footer(){
-  return (<div class="foot" id="foot"></div>)
+  return (
+  <div class="foot" id="foot">
+    <a id="profile-link" target="_blank" href="https://www.linkedin.com/in/trvr-benntt/">
+        <div id="linkedin" class="hide">My LinkedIn</div>
+        <img class="icon" src={linkedicon} alt="LinkedIn logo" />
+    </a>
+      <a id="profile-link" target="_blank" href="https://github.com/trvobennn">
+        <div id="git" class="hide">My GitHub</div>
+        <img class="icon" src={giticon} alt="Github logo" />
+      </a>
+  </div>)
 }
 
 
-const root = document.getElementById("app")
-ReactDOM.render(
+const rootElem = document.getElementById("root")
+const root = createRoot(rootElem);
+root.render(
   <div>
     <Headshot />
     <Mainbox />
     <Footer />
-  </div>
-  , root)
+  </div>)
